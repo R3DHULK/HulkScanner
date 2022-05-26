07e87500baa436c74a0d38066b3ffc721564f1c7
R3DHULK <105884682+R3DHULK@users.noreply.github.com>
2022-05-26 01:39:52 -0500

    Update hulkscannner.sh

--- hulkscannner.sh
+++ hulkscannner.sh
@@ -8,7 +8,7 @@ then
   echo "Usage: ./hulkscanner.sh [IP]"
   echo "Example ./hulkscanner.sh 192.168.1.10"
 else
-  echo "Please wait while it is scanning all the open ports..."
+  echo "Please wait while HULK is scanning all the open ports..."
   nc -nvz $1 1-65535 > $1.txt 2>&1
 fi
 tac $1.txt
